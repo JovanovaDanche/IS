bc6d5204747adde9951e2b428daa1f81644821e5
JovanovaDanche <jovanovadanche@gmail.com>
2024-04-30 01:08:16 +0200

    Add Lab4

---
+++ EShopAdminApplication/EShopAdminApplication/Models/EShopApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace EShopAdminApplication.Models
+{
+    public class EShopApplicationUser
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string? FirstName { get; set; }
+    }
+}
